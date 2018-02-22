2a959845b1fd9d984610cdeb017ee669c0cdfb92
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2018-02-22 10:56:02 -0500

    some tests added more needed

--- lab_03/funcs.cpp
+++ lab_03/funcs.cpp
@@ -124,15 +124,3 @@ string reverse_order(string beg, string end){
   fin.close();
   return ans;
 }
-
-/*
-int main(){
-
-  cout << east_storage("07/22/2016") << endl;
-  cout << mineast() << endl;
-  cout << maxeast() << endl;
-  cout << compare("09/13/2016","09/17/2016") << endl;
-  cout << reverse_order("09/13/2016","09/17/2016") << endl;
-  return 0;
-}
-*/
--- lab_03/main.cpp
+++ lab_03/main.cpp
@@ -6,10 +6,12 @@ int main()
 {
   std::cout << "This main is the real program\n";
   std::cout << "hello world\n" ;
-  double min;
 
-  min = mineast();
-  std::cout << "Minimum east is " << min << std::endl;
+  cout << east_storage("07/22/2016") << endl;
+  cout << mineast() << endl;
+  cout << maxeast() << endl;
+  cout << compare("09/13/2016","09/17/2016") << endl;
+  cout << reverse_order("09/13/2016","09/17/2016") << endl;
 
   return 0;
 }
