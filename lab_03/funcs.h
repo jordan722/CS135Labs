311d19f5bc18619d2fd1e4886d88bdaa997e127f
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2018-02-25 21:29:53 -0500

    replaced removed files

---
+++ lab_03/funcs.h
@@ -0,0 +1,7 @@
+using namespace std;
+
+double east_storage(string inp_date);
+double mineast();
+double maxeast();
+string compare(string beg,string end);
+string reverse_order(string beg, string end);
---
+++ lab_03/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "funcs.h"
+
+int main(){
+  cout << east_storage("07/22/2016") << endl;
+  cout << mineast() << endl;
+  cout << maxeast() << endl;
+  cout << compare("09/13/2016","09/17/2016") << endl;
+  cout << reverse_order("09/13/2016","09/17/2016") << endl;
+
+  return 0;
+}
