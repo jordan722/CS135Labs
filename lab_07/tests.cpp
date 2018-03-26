a4a90a525b1721f65ea12ddf8c2cc5626296d1df
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2018-03-25 21:41:08 -0400

    tests completed

--- lab_07/tests.cpp
+++ lab_07/tests.cpp
@@ -1,45 +1,35 @@
 #include <iostream>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "numbers.h"
+#include "ascii.h"
 #include "doctest.h"
 
 
 
-TEST_CASE("Testing IsDivisble"){
-  CHECK(isDivisible(100,25)==true);
-  CHECK(isDivisible(35,17)==false);
+TEST_CASE("ShiftChar"){
+  cout << "Testing shiftChar" << endl;
+  CHECK(shiftChar('c', 2)=='e');
+  CHECK(shiftChar('X', 27)=='Y');
+  CHECK(shiftChar('g', 10)=='q');
+  CHECK(shiftChar('e', 24)=='c');
 }
 
-TEST_CASE("Testing isPrime"){
-  CHECK(isPrime(2)==true);
-  CHECK(isPrime(3)==true);
-  CHECK(isPrime(35)==false);
-  CHECK(isPrime(17)==true);
+TEST_CASE("encryptCaesar"){
+  cout << "Testing encryptCaesar('Hello, World!', 10)" << endl;
+  CHECK(encryptCaesar("Hello, World!", 10)=="Rovvy, Gybvn!");
 }
 
-TEST_CASE("Testing nextPrime"){
-  CHECK(nextPrime(14)==17);
-  CHECK(nextPrime(17)==19);
+TEST_CASE("Testing decryptCaesar"){
+  cout << "Testing decryptCaesar('Rovvy, Gybvn!', 10)" << endl;
+  CHECK(decryptCaesar("Rovvy, Gybvn!", 10)=="Hello, World!");
 }
 
-TEST_CASE("Testing countPrimes"){
-  CHECK(countPrimes(2,17)==7);
-  CHECK(countPrimes(2,3)==2);
-  CHECK(countPrimes(5,19)==6);
+TEST_CASE("Testing encryptVigenere"){
+  cout << "Testing encryptVigenere('Hello, World!', 'cake')" << endl;
+  CHECK(encryptVigenere("Hello, World!", "cake")=="Jevpq, Wyvnd!");
 }
 
-TEST_CASE("Testing isTwinPrime"){
-  CHECK(isTwinPrime(29)==true);
-  CHECK(isTwinPrime(23)==false);
-}
+TEST_CASE("Testing decryptVigenere"){
+  cout << "decryptVigenere('Jevpq, Wyvnd!', 'cake')" << endl;
+  CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake")=="Hello, World!");
 
-TEST_CASE("Testing nextTwinPrime"){
-  CHECK(nextTwinPrime(17)==19);
-  CHECK(nextTwinPrime(3)==5);
-}
-
-TEST_CASE("Testing largestTwinPrime"){
-  CHECK(largestTwinPrime(5,18)==17);
-  CHECK(largestTwinPrime(1,31)==31);
-  CHECK(largestTwinPrime(14,16)==-1);
 }
