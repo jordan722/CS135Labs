0ca6d6a6c9961c7bf7ca519ab3ae6e7991226e83
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2018-05-07 07:56:04 -0400

    more tests and readme added

--- lab_10/tests.cpp
+++ lab_10/tests.cpp
@@ -10,3 +10,25 @@ TEST_CASE("MinutesSinceMidnight"){
   CHECK(minutesSinceMidnight({10,30})==630);
   CHECK(minutesSinceMidnight({0,30})==30);
 }
+
+
+TEST_CASE("minutesUntil"){
+  cout << "MinutesUntil" << endl;
+  Time now;
+  now.h = 9;
+  now.m = 58;
+  Time later;
+  later.h = 9;
+  later.m = 59;
+  CHECK(minutesUntil(now,later)==1);
+  now.h = 8;
+  now.m = 58;
+  later.h = 9;
+  later.m = 59;
+  CHECK(minutesUntil(now,later)==61);
+  now.h = 9;
+  now.m = 58;
+  later.h = 10;
+  later.m = 32;
+  CHECK(minutesUntil(now,later)==34);
+}
